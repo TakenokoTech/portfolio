fab915da0c524ef05a2284f8c1adc35f60b22e60
takenaka <https://github.com/TakenokoTech>
2023-01-05 15:36:13 +0900

    fixed android logo.

--- src/asset/string.ts
+++ src/asset/string.ts
@@ -37,7 +37,7 @@ export default {
     product: [
         {
             no: 1,
-            img: Image.icon,
+            img: Image.androbo,
             title: "android app",
             text: "blackboard app, notify app",
             url: "https://play.google.com/store/apps/dev?id=5785483366313980962&hl=ja"
@@ -94,5 +94,8 @@ export default {
     },
     fotter: {
         text: "© 2023 Takenoko."
+    },
+    caution: {
+        product: "The Android robot is reproduced or modified from work created and shared by Google and used according to terms described in the Creative Commons 3.0 Attribution License."
     }
 }
